8317332b0968241984959853f5d5d7430af70ee3
rndelpuerto <63263893+rndelpuerto@users.noreply.github.com>
2020-06-04 13:12:09 -0300

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,7 +9,7 @@ export default function Hero({ onQuery }) {
         <h2 className="hero__title jumbotron-heading h2">
           Your favourites movies. Explained
         </h2>
-        <p className="hero__subtitle  h3">
+        <p className="hero__subtitle  h3  mb-5">
           Figured out what happened. Then find out why.
         </p>
         <SearchBox onQuery={onQuery} />
